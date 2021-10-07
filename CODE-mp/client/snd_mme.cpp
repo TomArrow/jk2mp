4665995b82d631b2506f2617148d0fc0024e2cda
Tom Arrow <davorfilms@googlemail.com>
2021-10-08 00:30:36 +0200

    trying to fix audio, missing sounds atm

--- CODE-mp/client/snd_mme.cpp
+++ CODE-mp/client/snd_mme.cpp
@@ -4,11 +4,15 @@
 
 #include "snd_local.h"
 #include "snd_mix.h"
+#include <vector>
 
 #define MME_SNDCHANNELS 128
 #define MME_LOOPCHANNELS 128
 
 extern	cvar_t	*mme_saveWav;
+extern	cvar_t	*mme_rollingShutterPixels;
+extern	cvar_t	*mme_rollingShutterMultiplier;
+extern	std::vector<int> pboRollingShutterProgresses;
 
 typedef struct {
 	char			baseName[MAX_OSPATH];
@@ -98,85 +102,107 @@ Called from CL_Frame() in cl_main.c when shooting avidemo
 ===================
 */
 #define MAXUPDATE 4096
-void S_MMEUpdate( float scale ) {
-	int count, speed;
-	int mixTemp[MAXUPDATE*2];
-	short mixClip[MAXUPDATE*2];
+void S_MMEUpdate(float scale) {
+	
+	for (int i = 0; i < pboRollingShutterProgresses.size(); i++) {
+		if (pboRollingShutterProgresses[i] == 0) { // This is the first rolling shutter line/block of lines captured
 
-	if (!mmeSound.fileHandle && mme_saveWav->integer != 2)
-		return;
-	if (!mmeSound.gotFrame) {
-		if (mme_saveWav->integer != 2)
-			S_MMEWavClose();
-		return;
+
+			int count, speed;
+			int mixTemp[MAXUPDATE * 2];
+			short mixClip[MAXUPDATE * 2];
+
+			if (!mmeSound.fileHandle && mme_saveWav->integer != 2)
+				return;
+			if (!mmeSound.gotFrame) {
+				if (mme_saveWav->integer != 2)
+					S_MMEWavClose();
+				return;
+			}
+			count = (int)mmeSound.deltaSamples;
+			if (!count)
+				return;
+			mmeSound.deltaSamples -= count;
+			if (count > MAXUPDATE)
+				count = MAXUPDATE;
+
+			speed = (scale * (MIX_SPEED << MIX_SHIFT)) / MME_SAMPLERATE;
+			if (speed < 0 || (speed == 0 && scale))
+				speed = 1;
+
+			Com_Memset(mixTemp, 0, sizeof(int) * count * 2);
+			if (speed > 0) {
+				S_MixChannels(mmeSound.channels, MME_SNDCHANNELS, speed, count, mixTemp);
+				S_MixLoops(mmeSound.loops, MME_LOOPCHANNELS, speed, count, mixTemp);
+				S_MixEffects(&mmeSound.effect, speed, count, mixTemp);
+			}
+			S_MixClipOutput(count, mixTemp, mixClip, 0, MAXUPDATE - 1);
+			if (mme_saveWav->integer != 2) {
+				FS_Write(mixClip, count * 4, mmeSound.fileHandle);
+			}
+			else if (mme_saveWav->integer == 2) {
+				Com_Memcpy(&wavExportBuf[bytesInBuf], mixClip, count * 4);
+				bytesInBuf += count * 4;
+			}
+			mmeSound.fileSize += count * 4;
+			mmeSound.gotFrame = qfalse;
+
+		}
 	}
-	count = (int)mmeSound.deltaSamples;
-	if (!count)
-		return;
-	mmeSound.deltaSamples -= count;
-	if (count > MAXUPDATE)
-		count = MAXUPDATE;
-
-	speed = (scale * (MIX_SPEED << MIX_SHIFT)) / MME_SAMPLERATE;
-	if (speed < 0 || (speed == 0 && scale) )
-		speed = 1;
-
-	Com_Memset( mixTemp, 0, sizeof(int) * count * 2);
-	if ( speed > 0 ) {
-		S_MixChannels( mmeSound.channels, MME_SNDCHANNELS, speed, count, mixTemp );
-		S_MixLoops( mmeSound.loops, MME_LOOPCHANNELS, speed, count, mixTemp );
-		S_MixEffects( &mmeSound.effect, speed, count, mixTemp );
-	}	
-	S_MixClipOutput( count, mixTemp, mixClip, 0, MAXUPDATE - 1 );
-	if (mme_saveWav->integer != 2) {
-		FS_Write( mixClip, count*4, mmeSound.fileHandle );
-	} else if (mme_saveWav->integer == 2) {
-		Com_Memcpy(&wavExportBuf[bytesInBuf], mixClip, count*4 );
-		bytesInBuf += count*4;
-	}
-	mmeSound.fileSize += count * 4;
-	mmeSound.gotFrame = qfalse;
 }
 
 void S_MMERecord( const char *baseName, float deltaTime ) {
-	const char *format = Cvar_VariableString("mme_screenShotFormat");
-	const int shot = Cvar_VariableIntegerValue("mme_saveShot");
-	const int depth = Cvar_VariableIntegerValue("mme_saveDepth");
-	if (!mme_saveWav->integer || (mme_saveWav->integer == 2 && (Q_stricmp(format, "avi") || (!shot && !depth))))
-		return;
-	if (Q_stricmp(baseName, mmeSound.baseName) && mme_saveWav->integer != 2) {
-		char fileName[MAX_OSPATH];
-		if (mmeSound.fileHandle)
-			S_MMEWavClose();
-		Com_sprintf( fileName, sizeof(fileName), "%s.wav", baseName );
-		mmeSound.fileHandle = FS_FOpenFileReadWrite( fileName );
-		if (!mmeSound.fileHandle) {
-			mmeSound.fileHandle = FS_FOpenFileWrite( fileName );
-			if (!mmeSound.fileHandle) 
+
+	for (int i = 0; i < pboRollingShutterProgresses.size(); i++) {
+		if (pboRollingShutterProgresses[i] == 0) { // This is the first rolling shutter line/block of lines captured
+			
+			
+			
+			const char* format = Cvar_VariableString("mme_screenShotFormat");
+			const int shot = Cvar_VariableIntegerValue("mme_saveShot");
+			const int depth = Cvar_VariableIntegerValue("mme_saveDepth");
+			if (!mme_saveWav->integer || (mme_saveWav->integer == 2 && (Q_stricmp(format, "avi") || (!shot && !depth))))
 				return;
+			if (Q_stricmp(baseName, mmeSound.baseName) && mme_saveWav->integer != 2) {
+				char fileName[MAX_OSPATH];
+				if (mmeSound.fileHandle)
+					S_MMEWavClose();
+				Com_sprintf(fileName, sizeof(fileName), "%s.wav", baseName);
+				mmeSound.fileHandle = FS_FOpenFileReadWrite(fileName);
+				if (!mmeSound.fileHandle) {
+					mmeSound.fileHandle = FS_FOpenFileWrite(fileName);
+					if (!mmeSound.fileHandle)
+						return;
+				}
+				Q_strncpyz(mmeSound.baseName, baseName, sizeof(mmeSound.baseName));
+				mmeSound.deltaSamples = 0;
+				mmeSound.sampleRate = MME_SAMPLERATE;
+				FS_Seek(mmeSound.fileHandle, 0, FS_SEEK_END);
+				mmeSound.fileSize = FS_filelength(mmeSound.fileHandle);
+				if (mmeSound.fileSize < WAV_HEADERSIZE) {
+					int left = WAV_HEADERSIZE - mmeSound.fileSize;
+					while (left) {
+						FS_Write(&left, 1, mmeSound.fileHandle);
+						left--;
+					}
+					mmeSound.fileSize = WAV_HEADERSIZE;
+				}
+			}
+			else if (Q_stricmp(baseName, mmeSound.baseName) && mme_saveWav->integer == 2) {
+				Q_strncpyz(mmeSound.baseName, baseName, sizeof(mmeSound.baseName));
+				mmeSound.deltaSamples = 0;
+				mmeSound.sampleRate = MME_SAMPLERATE;
+				mmeSound.fileSize = 0;
+				bytesInBuf = 0;
+			}
+			mmeSound.deltaSamples += deltaTime * mmeSound.sampleRate;
+			mmeSound.gotFrame = qtrue;
+
+
 		}
-		Q_strncpyz( mmeSound.baseName, baseName, sizeof( mmeSound.baseName ));
-		mmeSound.deltaSamples = 0;
-		mmeSound.sampleRate = MME_SAMPLERATE;
-		FS_Seek( mmeSound.fileHandle, 0, FS_SEEK_END );
-		mmeSound.fileSize = FS_filelength( mmeSound.fileHandle );
-		if ( mmeSound.fileSize < WAV_HEADERSIZE) {
-			int left = WAV_HEADERSIZE - mmeSound.fileSize;
-			while (left) {
-				FS_Write( &left, 1, mmeSound.fileHandle );
-				left--;
-       		}
-			mmeSound.fileSize = WAV_HEADERSIZE;
-		}
-	} else if (Q_stricmp(baseName, mmeSound.baseName) && mme_saveWav->integer == 2) {
-		Q_strncpyz( mmeSound.baseName, baseName, sizeof( mmeSound.baseName ));
-		mmeSound.deltaSamples = 0;
-		mmeSound.sampleRate = MME_SAMPLERATE;
-		mmeSound.fileSize = 0;
-		bytesInBuf = 0;
 	}
-	mmeSound.deltaSamples += deltaTime * mmeSound.sampleRate;
-	mmeSound.gotFrame = qtrue;
+
+	
 }
 
 /* This is a seriously crappy hack, but it'll work for now */
