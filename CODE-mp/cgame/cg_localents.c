1922ebf2eea6cfff5c5121846578a2eb1bd1a0b5
Tom Arrow <davorfilms@googlemail.com>
2022-04-07 14:10:42 +0200

    Allow more local entities

--- CODE-mp/cgame/cg_localents.c
+++ CODE-mp/cgame/cg_localents.c
@@ -6,7 +6,7 @@
 
 #include "cg_local.h"
 
-#define	MAX_LOCAL_ENTITIES	2048 //ent: Raz: was 512
+#define	MAX_LOCAL_ENTITIES	16384 //ent: Raz: was 512 // was 2048. i want more - TA
 localEntity_t	cg_localEntities[MAX_LOCAL_ENTITIES];
 localEntity_t	cg_activeLocalEntities;		// double linked list
 localEntity_t	*cg_freeLocalEntities;		// single linked list
