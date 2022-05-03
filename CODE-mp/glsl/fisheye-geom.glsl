8f6abbd4a0ca67386b8b5756f28b8dffbbfae0b2
Tom Arrow <davorfilms@googlemail.com>
2022-05-03 23:56:46 +0200

    Moved equirectangular transformation over to geometry shader. YAY

--- CODE-mp/glsl/fisheye-geom.glsl
+++ CODE-mp/glsl/fisheye-geom.glsl
@@ -1,64 +1,149 @@
 // Geometry Shader
   #extension GL_ARB_geometry_shader4 : enable
+  //in float realDepth[3];
+float realDepth[3]; in vec4 color[3];
+out vec4 vertColor;
 
-  in float realDepth[3];
-  in vec4 color[3];
-  out vec4 vertColor;
+uniform vec3 dofJitterUniform;
+uniform float dofFocusUniform;
+uniform float dofRadiusUniform;
 
-  // --------------------
-  void main()
-  {
-    bool someInBack = false;
-    bool someLeft = false;
-    bool someRight = false;
-    for(int i = 0; i < 3; i++)
-    {
-        if(realDepth[i]>0) someInBack = true;
-        if(gl_PositionIn[i].x <= 0) someLeft = true;
-        if(gl_PositionIn[i].x > 0) someRight = true;
-    }
+float angleOnPlane(vec3 point, vec3 axis1, vec3 axis2)
+{
 
-    bool wrappedAround = false;
-    if(someInBack && someLeft && someRight){
-        wrappedAround = true;
-    }
+	vec2 planePosition = vec2(dot(point, axis1), dot(point, axis2));
+	return acos(dot(vec2(1, 0), normalize(planePosition)));
+}
 
-    if(!wrappedAround){
-        for(int i = 0; i < 3; i++)
-        {
-          gl_Position = gl_PositionIn[i];
-          gl_TexCoord[0] = gl_TexCoordIn[i][0];
-          vertColor = color[i]; 
-          EmitVertex();
-        }
-            EndPrimitive();
-    } else {
-        // Emit 2 separate vertices.
-        for(int i = 0; i < 3; i++)
-        {
-            vec4 thisPosition = gl_PositionIn[i];
-            if(realDepth[i]>0){
-                if(thisPosition.x <= 0) thisPosition.x+=2.0;
-            }
-            gl_Position = thisPosition;
-            gl_TexCoord[0] = gl_TexCoordIn[i][0];
-            vertColor = color[i]; 
-            EmitVertex();
-        }
-            EndPrimitive();
-        for(int i = 0; i < 3; i++)
-        {
-            vec4 thisPosition = gl_PositionIn[i];
-            if(realDepth[i]>0){
-                if(thisPosition.x > 0) thisPosition.x-=2.0;
-            }
-            gl_Position = thisPosition;
-            gl_TexCoord[0] = gl_TexCoordIn[i][0];
-            vertColor = color[i]; 
-            EmitVertex();
-        }
-            EndPrimitive();
-    }
-    
+vec3 getPerpendicularAxis(vec3 point, vec3 mainAxis)
+{
+	return point - dot(point, mainAxis) *mainAxis;
+}
 
-  }
+void setDebugColor(float x, float y, float z)
+{
+	//debugColor = vec3(max(0.0,min(1.0,x)),max(0.0,min(1.0,y)),max(0.0,min(1.0,z)));
+}
+
+float calcDofFactor(float pointDistance, float dofFocus)
+{
+	return abs(pointDistance - dofFocus);	// /pointDistance;
+}
+
+vec4 equirect_getPos(vec3 pointVec, int iter)
+{
+
+	float pi = radians(180);
+
+	vec3 axis[3];
+	axis[0] = vec3(0.0, 0.0, -1.0);
+	axis[1] = vec3(-1.0, 0.0, 0.0);
+	axis[2] = vec3(0.0, 1.0, 0.0);
+
+	float distance = length(pointVec);
+
+	vec3 perpendicularZAxisToPoint = getPerpendicularAxis(normalize(pointVec), axis[2].xyz);
+
+	// Apply DOF jitter
+	float dofFactor = calcDofFactor(distance, dofFocusUniform) *dofRadiusUniform *0.001;	// 0.001 to get it roughly in line with normal dof in mme. 
+	vec3 perpendicularXAxisToPoint = cross(axis[2].xyz, perpendicularZAxisToPoint);
+	pointVec.x += dofFactor* dot(dofJitterUniform, perpendicularXAxisToPoint);
+	pointVec.y += dofFactor* dot(dofJitterUniform, axis[2].xyz);
+	pointVec.z += dofFactor* dot(dofJitterUniform, perpendicularZAxisToPoint);
+
+	pointVec = normalize(pointVec);
+
+	vec4 outputPos;
+
+	float depth = dot(axis[0].xyz, pointVec);
+	realDepth[iter] = depth;
+
+	float xAngle = angleOnPlane(pointVec, -axis[1].xyz, axis[0].xyz) / pi;
+
+	float yAngle = angleOnPlane(pointVec, axis[2].xyz, perpendicularZAxisToPoint) / pi;
+
+	xAngle -= 0.5;
+	xAngle *= 2;
+	float widthSign = sign(xAngle);
+	xAngle = depth <= 0 ? xAngle : widthSign *(1.0 + (1.0 - abs(xAngle)));
+	xAngle *= 0.5;
+
+	yAngle -= 0.5;
+	yAngle *= 2;
+	float heightSign = sign(yAngle);
+
+	outputPos.x = xAngle;
+	outputPos.y = yAngle;
+	outputPos.z = 1.0 - 1.0 / distance;
+	outputPos.w = 1.0;
+
+	return outputPos;
+}
+
+void main()
+{
+	bool someInBack = false;
+	bool someLeft = false;
+	bool someRight = false;
+
+	vec4 positions[3];
+	for (int i = 0; i < 3; i++)
+	{
+		positions[i] = equirect_getPos(gl_PositionIn[i].xyz, i);
+	}
+
+	for (int i = 0; i < 3; i++)
+	{
+		if (realDepth[i] > 0) someInBack = true;
+		if (positions[i].x <= 0) someLeft = true;
+		if (positions[i].x > 0) someRight = true;
+	}
+
+	bool wrappedAround = false;
+	if (someInBack && someLeft && someRight)
+	{
+		wrappedAround = true;
+	}
+
+	if (!wrappedAround)
+	{
+		for (int i = 0; i < 3; i++)
+		{
+			gl_Position = positions[i];
+			gl_TexCoord[0] = gl_TexCoordIn[i][0];
+			vertColor = color[i];
+			EmitVertex();
+		}
+		EndPrimitive();
+	}
+	else
+	{
+		// Emit 2 separate vertices.
+		for (int i = 0; i < 3; i++)
+		{
+			vec4 thisPosition = positions[i];
+			if (realDepth[i] > 0)
+			{
+				if (thisPosition.x <= 0) thisPosition.x += 2.0;
+			}
+			gl_Position = thisPosition;
+			gl_TexCoord[0] = gl_TexCoordIn[i][0];
+			vertColor = color[i];
+			EmitVertex();
+		}
+		EndPrimitive();
+		for (int i = 0; i < 3; i++)
+		{
+			vec4 thisPosition = positions[i];
+			if (realDepth[i] > 0)
+			{
+				if (thisPosition.x > 0) thisPosition.x -= 2.0;
+			}
+			gl_Position = thisPosition;
+			gl_TexCoord[0] = gl_TexCoordIn[i][0];
+			vertColor = color[i];
+			EmitVertex();
+		}
+		EndPrimitive();
+	}
+}
--- CODE-mp/glsl/fisheye-vertex.glsl
+++ CODE-mp/glsl/fisheye-vertex.glsl
@@ -4,92 +4,93 @@ uniform float dofRadiusUniform;
 
 out vec3 debugColor;
 out vec4 color;
-out float realDepth;
+out float realDepth;float angleOnPlane(vec3 point, vec3 axis1, vec3 axis2)
+{
 
-float angleOnPlane(vec3 point, vec3 axis1, vec3 axis2){
-	
-	vec2 planePosition = vec2( dot(point,axis1), dot(point,axis2));
-	return acos(dot(vec2(1,0),normalize(planePosition)));
+	vec2 planePosition = vec2(dot(point, axis1), dot(point, axis2));
+	return acos(dot(vec2(1, 0), normalize(planePosition)));
 }
 
-vec3 getPerpendicularAxis(vec3 point, vec3 mainAxis){
-	return point - dot(point,mainAxis)*mainAxis;
+vec3 getPerpendicularAxis(vec3 point, vec3 mainAxis)
+{
+	return point - dot(point, mainAxis) *mainAxis;
 }
 
-void setDebugColor(float x,float y, float z){
+void setDebugColor(float x, float y, float z)
+{
 	//debugColor = vec3(max(0.0,min(1.0,x)),max(0.0,min(1.0,y)),max(0.0,min(1.0,z)));
 }
 
-
-float calcDofFactor(float pointDistance, float dofFocus){
-	return abs(pointDistance-dofFocus);// /pointDistance;
+float calcDofFactor(float pointDistance, float dofFocus)
+{
+	return abs(pointDistance - dofFocus);	// /pointDistance;
 }
 
-vec4 equirect_getPos(vec3 pointVec){
+vec4 equirect_getPos(vec3 pointVec)
+{
 
-  float pi =radians(180);
+	float pi = radians(180);
 
-  vec3 axis[3];
-  axis[0] = vec3(0.0,0.0,-1.0);
-  axis[1] = vec3(-1.0,0.0,0.0);
-  axis[2] = vec3(0.0,1.0,0.0);
+	vec3 axis[3];
+	axis[0] = vec3(0.0, 0.0, -1.0);
+	axis[1] = vec3(-1.0, 0.0, 0.0);
+	axis[2] = vec3(0.0, 1.0, 0.0);
 
-  float distance = length(pointVec);
+	float distance = length(pointVec);
 
-  vec3 perpendicularZAxisToPoint = getPerpendicularAxis(normalize(pointVec),axis[2].xyz);
+	vec3 perpendicularZAxisToPoint = getPerpendicularAxis(normalize(pointVec), axis[2].xyz);
 
-  // Apply DOF jitter
-  float dofFactor = calcDofFactor(distance,dofFocusUniform)*dofRadiusUniform*0.001; // 0.001 to get it roughly in line with normal dof in mme. 
-  vec3 perpendicularXAxisToPoint = cross( axis[2].xyz,perpendicularZAxisToPoint);
-  pointVec.x += dofFactor*dot(dofJitterUniform,perpendicularXAxisToPoint);
-  pointVec.y += dofFactor*dot(dofJitterUniform,axis[2].xyz);
-  pointVec.z += dofFactor*dot(dofJitterUniform,perpendicularZAxisToPoint);
+	// Apply DOF jitter
+	float dofFactor = calcDofFactor(distance, dofFocusUniform) *dofRadiusUniform *0.001;	// 0.001 to get it roughly in line with normal dof in mme. 
+	vec3 perpendicularXAxisToPoint = cross(axis[2].xyz, perpendicularZAxisToPoint);
+	pointVec.x += dofFactor* dot(dofJitterUniform, perpendicularXAxisToPoint);
+	pointVec.y += dofFactor* dot(dofJitterUniform, axis[2].xyz);
+	pointVec.z += dofFactor* dot(dofJitterUniform, perpendicularZAxisToPoint);
 
+	pointVec = normalize(pointVec);
 
-  pointVec = normalize(pointVec);
+	vec4 outputPos;
 
-  vec4 outputPos;
+	float depth = dot(axis[0].xyz, pointVec);
+	realDepth = depth;
 
-  float depth = dot(axis[0].xyz,pointVec);
-  realDepth = depth;
-  
-  float xAngle = angleOnPlane(pointVec,-axis[1].xyz,axis[0].xyz)/pi;
-  
-  float yAngle = angleOnPlane(pointVec,axis[2].xyz,perpendicularZAxisToPoint)/pi;
-    
-  xAngle -= 0.5;
-  xAngle *= 2;
-  float widthSign = sign(xAngle);
-  xAngle = depth<=0 ? xAngle : widthSign*(1.0+(1.0-abs(xAngle)));
-  xAngle *= 0.5;
+	float xAngle = angleOnPlane(pointVec, -axis[1].xyz, axis[0].xyz) / pi;
 
-  yAngle -= 0.5;
-  yAngle *= 2;
-  float heightSign = sign(yAngle);
+	float yAngle = angleOnPlane(pointVec, axis[2].xyz, perpendicularZAxisToPoint) / pi;
 
-  outputPos.x = xAngle;
-  outputPos.y = yAngle;
-  outputPos.z = 1.0-1.0/distance;
-  outputPos.w= 1.0;
+	xAngle -= 0.5;
+	xAngle *= 2;
+	float widthSign = sign(xAngle);
+	xAngle = depth <= 0 ? xAngle : widthSign *(1.0 + (1.0 - abs(xAngle)));
+	xAngle *= 0.5;
 
-  return outputPos;
+	yAngle -= 0.5;
+	yAngle *= 2;
+	float heightSign = sign(yAngle);
+
+	outputPos.x = xAngle;
+	outputPos.y = yAngle;
+	outputPos.z = 1.0 - 1.0 / distance;
+	outputPos.w = 1.0;
+
+	return outputPos;
 }
 
-void equirectangular(){
+void equirectangular()
+{
 
+	vec4 correctPos = gl_ModelViewMatrix * gl_Vertex;
+	vec3 pointVec = -correctPos.xyz;
+	//gl_Position = equirect_getPos(pointVec); // TODO Reinstate this IF geometry shader is not available.
+	gl_Position = vec4(pointVec, 1.0);
 
-  vec4 correctPos = gl_ModelViewMatrix * gl_Vertex;
-  vec3 pointVec = -correctPos.xyz;
-  gl_Position = equirect_getPos(pointVec);
+	gl_TexCoord[0] = gl_MultiTexCoord0;
 
-  gl_TexCoord[0] = gl_MultiTexCoord0;
-
-  color = gl_Color;
+	color = gl_Color;
 }
-
 
 void main(void)
-{   
+{
 	equirectangular();
-	
+
 }
